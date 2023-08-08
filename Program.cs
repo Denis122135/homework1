c5661bf45fc355be72874e6609b50997dbc802b1
Denis <www.00.www38@gmail.com>
2023-08-08 18:56:20 +0600

    fat

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Введите 1 число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите 2 число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int max = 0;
+// int min = 0;
+// Console.WriteLine();
+// if (num1 > num2)
+// {
+//     max = num1;
+//     min = num2;
+//     Console.WriteLine("Максимальное число");
+//     Console.WriteLine($"{max} ");
+//     Console.WriteLine("Минимальное число");
+//     Console.WriteLine($"{min} ");
+// }
+// else if (num1 < num2)
+// {
+//     max = num2;
+//     min = num1;
+//     Console.WriteLine("Максимальное число");
+//     Console.WriteLine($"{max} ");
+//     Console.WriteLine("Минимальное число");
+//     Console.WriteLine($"{min} ");
+// }
+// else
+//     Console.WriteLine("Числа равны");
+
+
+
+
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Введите 1 число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите 2 число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите 3 число");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = num1;
+
+// if (max < num2)
+// {
+//     max = num2;
+// }
+// if (max < num3)
+// {
+//     max = num3;
+// }
+// Console.WriteLine();
+// Console.WriteLine("Максимальное число");
+// Console.WriteLine($"{max} ");
+
+
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine("Введите число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 % 2 == 0)
+// {
+//     Console.WriteLine("Число четное");
+// }
+// else
+//     Console.WriteLine("Число нечетное");
+
+
+
+
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i = 2;
+
+// while (num >= i)
+// {
+//     Console.Write(i + " ");
+//     i = i + 2;
+// }
+
+
+
